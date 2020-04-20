7c5d69f8f8df4dca068a3e2b8f4f0c910177393b
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-04-20 14:38:17 +1000

    Foods controller file created

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,34 @@
+class FoodsController < ApplicationController
+    
+    def index
+        @foods = Food.all
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+end
