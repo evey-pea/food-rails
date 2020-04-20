0903c286d2d82b7ac1b58486156bfdcef3ef1a32
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-04-20 14:33:34 +1000

    Routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/foods", to: "foods#index", as: "food_index" 
+  post "/foods", to: "foods#create", as: "food_create"
+  get "/foods/new", to: "foods#new", as: "food_new"
+  get "/foods/:id", to: "foods#show", as: "food_show"
+  get "/foods/:id/edit", to: "foods#edit", as: "food_edit"
+  put "/foods/:id", to: "foods#update", as: "food_update"
+  delete "/foods/:id", to: "foods#destroy", as: "food_destroy"
 end
